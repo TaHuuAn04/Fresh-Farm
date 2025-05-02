6556415e9f13f2158bee5db72a6944f881383a18
TaHuuAn04 <an.taduychinh@hcmut.edu.vn>
2025-05-02 17:00:08 +0700

    feat: update streaming-not good

--- backend/src/modules/chat-bot/chat-bot.controller.ts
+++ backend/src/modules/chat-bot/chat-bot.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Inject, Post, Res, UseGuards } from '@nestjs/common';
 import { IChatBotService } from './chat-bot.interface';
 import { INJECTION_TOKEN } from '@common/enums/injection-token';
 import {
@@ -11,6 +11,7 @@ import { ChatAiResponseDto, MessageDto } from './dtos/chat.dto';
 import JwtAuthGuard from '@modules/auth/guard/jwtAuth.guard';
 import { CurrentUser } from '@common/decorators/current-user.decorator';
 import { User } from '@entities';
+import { Response } from 'express';
 
 @ApiTags('ChatBot')
 @UseGuards(JwtAuthGuard)
@@ -33,5 +34,31 @@ export class ChatBotController {
     @CurrentUser() user: User,
   ): Promise<ChatAiResponseDto> {
     return this.chatBotService.chatMessageBlocking(body.query, user.id);
+  }
+
+  @Post('streaming')
+  @ApiOperation({ summary: 'Chat message streaming' })
+  @ApiResponse({
+    status: 200,
+    description: 'Chat message streaming',
+  })
+  async chatMessageStreaming(
+    @Body() body: MessageDto,
+    @CurrentUser() user: User,
+    @Res() res: Response,
+  ): Promise<void> {
+    const result = await this.chatBotService.chatMessageStreaming(
+      body.query,
+      user.id,
+    );
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    result.pipe(res);
+
+    result.on('end', () => {
+      res.end();
+    });
   }
 }
